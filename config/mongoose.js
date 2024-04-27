5a74310bd3e5550b27b141751d38949d091a5940
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-04-27 09:40:26 +0600

    added schema and models for db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,50 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const URL = `${process.env.MONGO_URL}/${process.env.MONGO_DB_NAME}`;
 
-const mongooseDB = await mongoose.connect(URL);
+await mongoose.connect(URL);
 
-module.exports = mongooseDB;
+const SpotsSchema = new mongoose.Schema({
+  image: String,
+  touristSpotName: String,
+  countryName: String,
+  location: String,
+  shortDescription: String,
+  averageCost: String,
+  seasonality: String,
+  travelTime: String,
+  totalVisitorsPerYear: String,
+});
+
+const UserSpotListSchema = new mongoose.Schema({
+  uuid: String,
+  userEmail: String,
+  userName: String,
+  spotList: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "AllSpots",
+    },
+  ],
+});
+
+const CountriesSchema = new mongoose.Schema({
+  countryName: String,
+  countryDescription: String,
+  countryImage: String,
+});
+
+const AllSpots = mongoose.model("AllSpots", SpotsSchema);
+const UserSpotList = mongoose.model("UserSpotList", UserSpotListSchema);
+const Countries = mongoose.model("Countries", CountriesSchema);
+
+const DbModels = {
+  AllSpots,
+  UserSpotList,
+  Countries,
+};
+
+module.exports = DbModels;
